04b239c3aea840827bd3d4ce92b6b0470c2d3f5b
camthalion0 <camthalion1234@gmail.com>
2019-07-03 19:26:33 +0800

    first commit

---
+++ precache-manifest.e2e91d2c7acaff5dbc2b1093119a9e64.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160deaf9ebbacb703f199090ad92a6b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17eb3d3cd1059ab07f32",
+    "url": "./static/css/main.dbc35ac3.chunk.css"
+  },
+  {
+    "revision": "831f2121f8ebafe4d44f",
+    "url": "./static/js/2.11a0536e.chunk.js"
+  },
+  {
+    "revision": "17eb3d3cd1059ab07f32",
+    "url": "./static/js/main.5c99d329.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a077bc16110ed84ec972dde6434057cf",
+    "url": "./static/media/About_background.a077bc16.jpg"
+  },
+  {
+    "revision": "3572c77fd4339a21b963ec7e03abc37f",
+    "url": "./static/media/Hello_background.3572c77f.jpg"
+  },
+  {
+    "revision": "859d1dcf2574f7753c375ebd3c402ef5",
+    "url": "./static/media/Skills_background.859d1dcf.jpg"
+  },
+  {
+    "revision": "d7c9ffb18572d4689b2b73c3690163dc",
+    "url": "./static/media/angostur.d7c9ffb1.ttf"
+  },
+  {
+    "revision": "f471c46fd43b1d46857f32b6176b7aef",
+    "url": "./static/media/heart2.f471c46f.png"
+  },
+  {
+    "revision": "18ef312777839854a8e51c92de779ec2",
+    "url": "./static/media/mail.18ef3127.png"
+  }
+]);
